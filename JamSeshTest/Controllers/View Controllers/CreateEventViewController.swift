2b80e4f6cd69e25af8fa299168e9a6f8ee511e0b
tanmanroberts <tanmanroberts@gmail.com>
2021-06-25 20:37:08 -0700

    changed the date label to a datepicker that shows calendar day and the time. Date is displayed as a string on the detail view. all of the basic functionality is there and functioning.

--- JamSeshTest/Controllers/View Controllers/CreateEventViewController.swift
+++ JamSeshTest/Controllers/View Controllers/CreateEventViewController.swift
@@ -12,40 +12,55 @@ class CreateEventViewController: UIViewController {
     //MARK: - Outlets
     @IBOutlet weak var eventNameTextField: UITextField!
     @IBOutlet weak var eventLocationTextField: UITextField!
-    @IBOutlet weak var eventTimeTextField: UITextField!
+    @IBOutlet weak var eventTimeTextField: UITextField! //this text field isnt in use rn. I deleted the connection
     @IBOutlet weak var instrumentsUsedTextField: UITextField!
     @IBOutlet weak var eventDetailsTextView: UITextView!
+    @IBOutlet weak var eventDatePicker: UIDatePicker!
     
     
     
     //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+
     }
+    
+    //MARK: - Actions
+    @IBAction func datePickerAction(_ sender: Any) {
+        
+        //for whatever reason this needs to exist even if it does nothing lol
+    }
+    
+    
+    
+    
+    
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         var vc = segue.destination as! EventDetailViewController
         vc.finalName = self.name
         vc.finalLocation = self.location
-        vc.finalTime = self.time
+        vc.finalTime =  "\(self.selectedDate)"
         vc.finalInstuments = self.instruments
         vc.finalDetails = self.details
     }
     
     //MARK: - Properties
+    var events: Event?
+    var selectedDate = Date()
+    
     var name = ""
     var location = ""
-    var time = ""
     var instruments = ""
     var details = ""
-    
+    var datePicker = UIDatePicker()
     //MARK: - Actions
     
     
     @IBAction func saveEventButtonTapped(_ sender: UITextField) {
         self.name = eventNameTextField.text!
         self.location = eventLocationTextField.text!
-        self.time = eventTimeTextField.text!
+        self.selectedDate = self.eventDatePicker.date
         self.instruments = instrumentsUsedTextField.text!
         self.details = eventDetailsTextView.text
         performSegue(withIdentifier: "toDetailVC", sender: self)
